f40dee6b8c1ae05e6c9166dedce75a1d726240ba
Easit AB <github@easit.com>
2020-09-22 20:59:59 +0200

    Updates

--- build/generateAndUpdateDocs.ps1
+++ build/generateAndUpdateDocs.ps1
@@ -1,9 +1,3 @@
-try {
-        Install-Module -Name platyPS -Scope CurrentUser -Force -ErrorAction Stop
-        Import-Module platyPS -Force -ErrorAction Stop
-} catch {
-        Write-Error $_ 
-}
 $projectRoot = Split-Path -Path $PSScriptRoot -Parent
 Write-Output "Project root: $projectRoot"
 Set-Location -Path $projectRoot
@@ -16,13 +10,25 @@ $allscripts += $privScripts
 $allscripts += $pubScripts
 $tempModuleFileName = 'MyModule'
 $tempModulePath = "$projectRoot/${tempModuleFileName}.psm1"
-try {
-    New-Item -Path $projectRoot -ItemType File -Name "${tempModuleFileName}.psm1" -ErrorAction Stop
-} catch {
-    Write-Error $_
-    break
-}
-foreach ($script in $allscripts) {
+
+New-Module -Name "$env:moduleName" -ScriptBlock {
+    $projectRoot = Split-Path -Path $PSScriptRoot -Parent
+    Write-Output "Project root: $projectRoot"
+    Set-Location -Path $projectRoot
+    Write-Output "Working in $projectRoot"
+    $privScripts = Get-ChildItem -Path "$projectRoot\src\priv" -Filter "*.ps1" -Recurse
+    $pubScripts = Get-ChildItem -Path "$projectRoot\src\pub" -Filter "*.ps1" -Recurse
+    $docsRoot = "$projectRoot/docs"
+    $allscripts = @()
+    $allscripts += $privScripts
+    $allscripts += $pubScripts
+    $tempModuleFileName = 'MyModule'
+    $tempModulePath = "$projectRoot/${tempModuleFileName}.psm1"
+    if (!(Test-Path -Path $tempModulePath)) {
+        $tempModuleFile = New-Item -Path "$projectRoot" -Name "${tempModuleFileName}.psm1" -ItemType "file"
+        Write-Output "Created $newModuleFile"
+    }
+    foreach ($script in $allscripts) {
         $exportFunction = "Export-ModuleMember -Function $($script.BaseName)"
         $scriptContent = Get-Content -Path "$($script.FullName)" -Raw
         if (Test-Path -Path $tempModulePath) {    
@@ -39,8 +45,30 @@ foreach ($script in $allscripts) {
                 break
             }
         }
+    }
 }
-Write-Output "Temp module file done"
+
+$manifest = @{
+    Path              = "$projectRoot\${tempModuleFileName}.psd1" 
+    RootModule        = "$tempModuleFileName.psm1" 
+    CompanyName       = "$env:companyName"
+    Author            = "$env:moduleAuthor"
+    ModuleVersion     = "$env:APPVEYOR_BUILD_VERSION"
+    HelpInfoUri       = "$env:projectUri/tree/development/docs"
+    LicenseUri        = "$env:projectUri/blob/development/LICENSE"
+    ProjectUri        = "$env:projectUri"
+    Description       = 'Management Framework for Easit BPS and Easit GO'
+    PowerShellVersion = '5.1'
+    Copyright         = "(c) 2020 $env:companyName. All rights reserved."
+}
+New-ModuleManifest @manifest | Out-Null
+
+try {
+        Install-Module -Name platyPS -Scope CurrentUser -Force -ErrorAction Stop
+        Import-Module platyPS -Force -ErrorAction Stop
+} catch {
+        Write-Error $_ 
+}
 try {
     Import-Module -Name "$projectRoot" -Force -Verbose
 } catch {
