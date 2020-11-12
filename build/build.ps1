ac7ecc9c997704ada3d1a8ff00fefc52d9f2d895
Easit AB <github@easit.com>
2020-11-12 09:56:54 +0100

    Updates to build and test processes

--- build/build.ps1
+++ build/build.ps1
@@ -1,40 +1,40 @@
 New-Module -Name "$env:moduleName" -ScriptBlock {
-    $modulePath = "$env:resourceRoot\$env:moduleName\$env:moduleName.psm1"
-    $privScripts = Get-ChildItem -Path "$env:resourceRoot\src\priv" -Filter "*.ps1" -Recurse
-    $pubScripts = Get-ChildItem -Path "$env:resourceRoot\src\pub" -Filter "*.ps1" -Recurse
+    $modulePath = "$env:sourceRoot\$env:moduleName.psm1"
+    $privScripts = Get-ChildItem -Path "$env:sourceRoot\private" -Filter "*.ps1" -Recurse
+    $pubScripts = Get-ChildItem -Path "$env:sourceRoot\public" -Filter "*.ps1" -Recurse
     if (!(Test-Path -Path $modulePath)) {
-        if (!(Test-Path -Path "$env:resourceRoot\$env:moduleName")) {
-            New-Item -Path "$env:resourceRoot" -Name "$env:moduleName" -ItemType "directory" | Out-Null
-            Write-Host "Created $env:resourceRoot\$env:moduleName"
-        }
-        $newModuleFile = New-Item -Path "$env:resourceRoot\$env:moduleName" -Name "$env:moduleName.psm1" -ItemType "file"
+        $newModuleFile = New-Item -Path "$env:sourceRoot" -Name "$env:moduleName.psm1" -ItemType "file"
         Write-Host "Created $newModuleFile"
     }
     foreach ($privateScript in $privScripts) {
-        $scriptName = "$($privateScript.Name)"
-        $scriptContent = Get-Content -Path "$env:resourceRoot\$scriptName" -Raw
+        $scriptContent = Get-Content -Path "$($privateScript.FullName)" -Raw
         if (Test-Path -Path $modulePath) {
             Add-Content -Path $modulePath -Value $scriptContent
+        } else {
+            Write-Host "Unable to find modulePath at $modulePath" -ForegroundColor Red
         }
     }
     foreach ($publicScript in $pubScripts) {
         $exportFunction = "Export-ModuleMember -Function $($publicScript.BaseName)"
         $scriptName = "$($publicScript.Name)"
-        $scriptContent = Get-Content -Path "$env:resourceRoot\$scriptName" -Raw
+        "$($publicScript.FullName)"
+        $scriptContent = Get-Content -Path "$($publicScript.FullName)" -Raw
         if (Test-Path -Path $modulePath) {            
             Add-Content -Path $modulePath -Value $exportFunction
+        } else {
+            Write-Host "Unable to find modulePath at $modulePath" -ForegroundColor Red
         }
     }
 }
 
 $manifest = @{
-    Path              = "$env:resourceRoot\$env:moduleName\$env:moduleName.psd1" 
+    Path              = "$env:sourceRoot\$env:moduleName.psd1" 
     RootModule        = "$env:moduleName.psm1" 
     CompanyName       = "$env:companyName"
     Author            = "$env:moduleAuthor"
     ModuleVersion     = "$env:APPVEYOR_BUILD_VERSION"
-    HelpInfoUri       = "$env:projectUri/tree/development/docs"
-    LicenseUri        = "$env:projectUri/blob/development/LICENSE"
+    HelpInfoUri       = "$env:helpInfoUri"
+    LicenseUri        = "$env:licenseUri"
     ProjectUri        = "$env:projectUri"
     Description       = 'Management Framework for Easit BPS and Easit GO'
     PowerShellVersion = '5.1'
