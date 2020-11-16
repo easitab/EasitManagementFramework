e09a8e17c97d84fc9c74be129861d75c44cef45e
Easit AB <github@easit.com>
2020-11-16 11:48:06 +0100

    final test

--- build/build_appveyor.ps1
+++ build/build_appveyor.ps1
@@ -1,25 +1,12 @@
-param (
-    [string]$SourceRoot,
-    [string]$ModuleName,
-    [string]$CompanyName,
-    [string]$ModuleAuthor,
-    [string]$Version,
-    [string]$HelpInfoUri,
-    [string]$LicenseUri
-)
-$moduleArgs = "$SourceRoot", "$ModuleName"
-New-Module -Name "$ModuleName" -ScriptBlock {
-    param([string[]]$moduleParams)
-    Write-Output "$moduleParams"
-    $moduleSourceRoot = $moduleParams[0]
-    Write-Output "$moduleSourceRoot"
-    $moduleModuleName = $moduleParams[1]
-    Write-Output "$moduleModuleName"
-    $modulePath = "$moduleSourceRoot\$moduleModuleName.psm1"
-    $privScripts = Get-ChildItem -Path "$moduleSourceRoot\private" -Filter "*.ps1" -Recurse
-    $pubScripts = Get-ChildItem -Path "$moduleSourceRoot\public" -Filter "*.ps1" -Recurse
+# This file in it self is not used but only a template.
+# Actual code that will be executed lives in appveyor.yml
+
+New-Module -Name "$env:moduleName" -ScriptBlock {
+    $modulePath = "$env:sourceRoot\$env:moduleName.psm1"
+    $privScripts = Get-ChildItem -Path "$env:sourceRoot\private" -Filter "*.ps1" -Recurse
+    $pubScripts = Get-ChildItem -Path "$env:sourceRoot\public" -Filter "*.ps1" -Recurse
     if (!(Test-Path -Path $modulePath)) {
-        New-Item -Path "$moduleSourceRoot" -Name "$moduleModuleName.psm1" -ItemType "file"
+        New-Item -Path "$env:sourceRoot" -Name "$env:moduleName.psm1" -ItemType "file"
     }
     foreach ($privateScript in $privScripts) {
         $scriptContent = Get-Content -Path "$($privateScript.FullName)" -Raw
@@ -38,19 +25,19 @@ New-Module -Name "$ModuleName" -ScriptBlock {
             Write-Output "Unable to find modulePath at $modulePath" -ForegroundColor Red
         }
     }
-} -ArgumentList $moduleArgs
-Get-ChildItem -Path "$SourceRoot"
+}
+Get-ChildItem -Path "$env:sourceRoot"
 $manifest = @{
-    Path              = "$SourceRoot\$ModuleName.psd1" 
-    RootModule        = "$ModuleName.psm1" 
-    CompanyName       = "$CompanyName"
-    Author            = "$ModuleAuthor"
-    ModuleVersion     = "$Version"
-    HelpInfoUri       = "$HelpInfoUri"
-    LicenseUri        = "$LicenseUri"
-    ProjectUri        = "$ProjectUri"
+    Path              = "$env:sourceRoot\$env:moduleName.psd1" 
+    RootModule        = "$env:moduleName.psm1" 
+    CompanyName       = "$env:companyName"
+    Author            = "$env:moduleAuthor"
+    ModuleVersion     = "$env:APPVEYOR_BUILD_VERSION"
+    HelpInfoUri       = "$env:helpInfoUri"
+    LicenseUri        = "$env:licenseUri"
+    ProjectUri        = "$env:projectUri"
     Description       = 'Management Framework for Easit BPS and Easit GO'
     PowerShellVersion = '5.1'
-    Copyright         = "(c) 2020 $CompanyName. All rights reserved."
+    Copyright         = "(c) 2020 $env:companyName. All rights reserved."
 }
 New-ModuleManifest @manifest
