2a251e5bd3d163c07885259ee141d90183478433
easitab <easitab@users.noreply.github.com>
2021-03-07 02:23:47 +0000

    Canary build

--- module/canaryBuild/EasitManagementFramework.psm1
+++ module/canaryBuild/EasitManagementFramework.psm1
@@ -1,3 +1,112 @@
+function Convert-EasitLogEntryToPsObject {
+    [CmdletBinding(HelpURI="https://github.com/easitab/EasitManagementFramework/blob/main/docs/v1/Convert-EasitLogEntryToPsObject.md")]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [string] $String,
+        [Parameter()]
+        [string] $Source
+    )
+    
+    begin {
+        Write-Verbose "$($MyInvocation.MyCommand) initialized"
+    }
+    process {
+        Write-Verbose "Selecting strings from entry"
+        $stringDate = Select-String -InputObject $String -Pattern '\d{4}-\d{2}-\d{2}'
+        $stringDate = "$($stringDate.Matches.Value)"
+        Write-Debug "stringDate = $stringDate"
+
+        $stringTime = Select-String -InputObject $String -Pattern '\d{2}:\d{2}:\d{2}\.\d{3}'
+        $stringTime = "$($stringTime.Matches.Value)"
+        Write-Debug "stringTime = $stringTime"
+
+        $stringLevel = Select-String -InputObject $String -Pattern 'FATAL|ERROR|WARN|INFO|DEBUG|TRACE'
+        $stringLevel = "$($stringLevel.Matches.Value)"
+        Write-Debug "stringLevel = $stringLevel"
+
+        $stringMessage = Select-String -InputObject $String -Pattern '- .+\['
+        $stringMessage = "$($stringMessage.Matches.Value)"
+        $stringMessage = $stringMessage.TrimStart('- ')
+        $stringMessage = $stringMessage.TrimEnd('[')
+        Write-Debug "stringMessage = $stringMessage"
+
+        $stringClass = Select-String -InputObject $String -Pattern '\[[\w\.]+\]$'
+        $stringClass = "$($stringClass.Matches.Value)"
+        $stringClass = $stringClass.TrimStart('[')
+        $stringClass = $stringClass.TrimEnd(']')
+        Write-Debug "stringClass = $stringClass"
+
+        if (Select-String -InputObject $String -Pattern '\]\s?(\r|\n)+' -Quiet) {
+            $stringStack = Select-String -InputObject $String -Pattern '\- [\w|\W|\n\r]*'
+            $stringStack = "$($stringStack.Matches.Value)"
+            $stringStack = $stringStack.TrimStart("- $stringMessage [$stringClass]")
+            $stringStack = $stringStack.TrimStart()
+            Write-Debug "stringStack = $stringStack"
+        } else {
+            $stringStack = $null
+        }
+
+        Write-Verbose "Creating object from entry strings"
+        $returnObject = [PSCustomObject]@{
+            Date                = "$stringDate"
+            Time                = "$stringTime"
+            Level               = "$stringLevel"
+            Class               = "$stringClass"
+            Message             = "$stringMessage"
+            FullStackMessage    = "$stringStack"
+            Source              = "$Source"
+        }
+        Write-Verbose "Returning entry as object"
+        return $returnObject
+    }
+    end {
+        Write-Verbose "$($MyInvocation.MyCommand) completed"
+    }
+}
+function Import-EMFXMLData {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string] $Path,
+
+        [Parameter()]
+        [switch] $Validate
+    )
+    
+    begin {
+        Write-Verbose "$($MyInvocation.MyCommand) initialized"
+    }
+    
+    process {
+        Write-Verbose "Process block start"
+        $xml = New-Object System.Xml.XmlDocument
+        try {
+            $xml.Load($Path)
+            Write-Verbose 'Loaded XML-file to XML-object'
+        } catch {
+            throw $_
+        }
+        if ($Validate) {
+            try {
+                Test-EMFXMLData -Path $Path
+                Write-Verbose 'XML validated successfully'
+            } catch {
+                throw $_
+            }
+        } else {
+            Write-Verbose "Skipping validation"
+        }
+        
+        
+        return $xml
+        Write-Verbose "Process block end"
+    }
+    
+    end {
+        Write-Verbose "$($MyInvocation.MyCommand) completed"
+    }
+}
+
 function Disable-EasitScheduledTask {
     [CmdletBinding()]
     param (
@@ -1013,115 +1122,6 @@ function Test-EMFXMLData {
     
     end {
         $schemaReader.Close()
-        Write-Verbose "$($MyInvocation.MyCommand) completed"
-    }
-}
-
-function Convert-EasitLogEntryToPsObject {
-    [CmdletBinding(HelpURI="https://github.com/easitab/EasitManagementFramework/blob/main/docs/v1/Convert-EasitLogEntryToPsObject.md")]
-    param (
-        [Parameter(ValueFromPipeline)]
-        [string] $String,
-        [Parameter()]
-        [string] $Source
-    )
-    
-    begin {
-        Write-Verbose "$($MyInvocation.MyCommand) initialized"
-    }
-    process {
-        Write-Verbose "Selecting strings from entry"
-        $stringDate = Select-String -InputObject $String -Pattern '\d{4}-\d{2}-\d{2}'
-        $stringDate = "$($stringDate.Matches.Value)"
-        Write-Debug "stringDate = $stringDate"
-
-        $stringTime = Select-String -InputObject $String -Pattern '\d{2}:\d{2}:\d{2}\.\d{3}'
-        $stringTime = "$($stringTime.Matches.Value)"
-        Write-Debug "stringTime = $stringTime"
-
-        $stringLevel = Select-String -InputObject $String -Pattern 'FATAL|ERROR|WARN|INFO|DEBUG|TRACE'
-        $stringLevel = "$($stringLevel.Matches.Value)"
-        Write-Debug "stringLevel = $stringLevel"
-
-        $stringMessage = Select-String -InputObject $String -Pattern '- .+\['
-        $stringMessage = "$($stringMessage.Matches.Value)"
-        $stringMessage = $stringMessage.TrimStart('- ')
-        $stringMessage = $stringMessage.TrimEnd('[')
-        Write-Debug "stringMessage = $stringMessage"
-
-        $stringClass = Select-String -InputObject $String -Pattern '\[[\w\.]+\]$'
-        $stringClass = "$($stringClass.Matches.Value)"
-        $stringClass = $stringClass.TrimStart('[')
-        $stringClass = $stringClass.TrimEnd(']')
-        Write-Debug "stringClass = $stringClass"
-
-        if (Select-String -InputObject $String -Pattern '\]\s?(\r|\n)+' -Quiet) {
-            $stringStack = Select-String -InputObject $String -Pattern '\- [\w|\W|\n\r]*'
-            $stringStack = "$($stringStack.Matches.Value)"
-            $stringStack = $stringStack.TrimStart("- $stringMessage [$stringClass]")
-            $stringStack = $stringStack.TrimStart()
-            Write-Debug "stringStack = $stringStack"
-        } else {
-            $stringStack = $null
-        }
-
-        Write-Verbose "Creating object from entry strings"
-        $returnObject = [PSCustomObject]@{
-            Date                = "$stringDate"
-            Time                = "$stringTime"
-            Level               = "$stringLevel"
-            Class               = "$stringClass"
-            Message             = "$stringMessage"
-            FullStackMessage    = "$stringStack"
-            Source              = "$Source"
-        }
-        Write-Verbose "Returning entry as object"
-        return $returnObject
-    }
-    end {
-        Write-Verbose "$($MyInvocation.MyCommand) completed"
-    }
-}
-function Import-EMFXMLData {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string] $Path,
-
-        [Parameter()]
-        [switch] $Validate
-    )
-    
-    begin {
-        Write-Verbose "$($MyInvocation.MyCommand) initialized"
-    }
-    
-    process {
-        Write-Verbose "Process block start"
-        $xml = New-Object System.Xml.XmlDocument
-        try {
-            $xml.Load($Path)
-            Write-Verbose 'Loaded XML-file to XML-object'
-        } catch {
-            throw $_
-        }
-        if ($Validate) {
-            try {
-                Test-EMFXMLData -Path $Path
-                Write-Verbose 'XML validated successfully'
-            } catch {
-                throw $_
-            }
-        } else {
-            Write-Verbose "Skipping validation"
-        }
-        
-        
-        return $xml
-        Write-Verbose "Process block end"
-    }
-    
-    end {
         Write-Verbose "$($MyInvocation.MyCommand) completed"
     }
 }
